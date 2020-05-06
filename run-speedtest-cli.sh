79885a4afb34f537e9c57b1bf76298968824414e
Wolfgang Huber <wolfgang.huber@embl.de>
2020-05-06 12:57:47 +0200

    Now also reports failure.

--- run-speedtest-cli.sh
+++ run-speedtest-cli.sh
@@ -1,2 +1,12 @@
-#/bin/sh
-/usr/local/bin/speedtest-cli --csv >> /Users/whuber/Dropbox/speedtest/`hostname -s`-speedtest.csv
+#/bin/bash
+RES=`/usr/local/bin/speedtest-cli --csv 2> /dev/null`  
+echo $RES
+if [ -z "$RES" ]
+then
+  RES="NA,NA,NA,`gdate --utc +%FT%TZ`,NA,NA,NA,NA,NA,NA"
+fi
+echo $RES >> /Users/whuber/Dropbox/speedtest/`hostname -s`-speedtest.csv
+
+# CSV column headers:
+# [1] "Server ID"   "Sponsor"     "Server Name" "Timestamp"   "Distance"
+# [6] "Ping"        "Download"    "Upload"      "Share"       "IP Address" if
