fab72ba3970d36ff4a4851e1e6e29f2ffc291a2a
Wolfgang Huber <wolfgang.huber@embl.de>
2020-05-14 19:27:44 +0200

    Trying to deal more gracefully with timeouts

--- run-speedtest-cli.sh
+++ run-speedtest-cli.sh
@@ -2,9 +2,9 @@
 RES=`/usr/local/bin/speedtest-cli --csv 2> /dev/null`  
 if [ -z "$RES" ]
 then
-  RES="NA,NA,NA,`gdate --utc +%FT%TZ`,NA,NA,NA,NA,NA,NA"
+  RES="NA,NA,NA,`/usr/local/bin/gdate --utc +%FT%TZ`,NA,NA,NA,NA,NA,NA"
 fi
-echo $RES >> /Users/whuber/Dropbox/speedtest/`hostname -s`-speedtest.csv
+echo "$RES">> /Users/whuber/Dropbox/speedtest/`hostname -s`-speedtest.csv
 
 # CSV column headers:
 # [1] "Server ID"   "Sponsor"     "Server Name" "Timestamp"   "Distance"
