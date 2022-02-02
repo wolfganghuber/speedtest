bba63b26371762879a396dcb00f30cae5d1ab002
Wolfgang Huber <wolfgang.huber@embl.org>
2022-02-02 18:46:28 +0100

    Updates to new paths

--- run-speedtest-cli.sh
+++ run-speedtest-cli.sh
@@ -1,10 +1,10 @@
 #/bin/bash
-RES=`/usr/local/bin/speedtest-cli --csv 2> /dev/null`  
+RES=`/opt/homebrew/bin/speedtest-cli --csv 2> /dev/null`  
 if [ -z "$RES" ]
 then
-  RES="NA,NA,NA,`/usr/local/bin/gdate --utc +%FT%TZ`,NA,NA,NA,NA,NA,NA"
+  RES="NA,NA,NA,`/opt/homebrew/bin/gdate --utc +%FT%TZ`,NA,NA,NA,NA,NA,NA"
 fi
-echo "$RES">> /Users/whuber/Dropbox/speedtest/speedtest-`hostname -s`.csv
+echo "$RES">> /Users/whuber/speedtest/speedtest-`hostname -s`.csv
 
 # CSV column headers:
 # [1] "Server ID"   "Sponsor"     "Server Name" "Timestamp"   "Distance"
